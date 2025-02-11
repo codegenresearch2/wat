9b8f6d4487084d357b3a89d299460d2c5fe9a394
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-11 20:56:34 +0000

    Reset to f46a09dcea19d8c6e003aaef34e4c49651e7c798 and made changes for LLM Round 1

--- docs/demo/run-demo-inspect.py
+++ docs/demo/run-demo-inspect.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python3
 import wat
 from livekey import animate_commands
-import re
 
 commands = [
     'python',
     'import wat',
     'wat / {7}',
     'wat.short / (1,)',
-    'import re',
     'wat()',
     'wat / re',
     'wat.code / re.match',
