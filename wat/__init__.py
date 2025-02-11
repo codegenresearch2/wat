8ed313e0a91019e0c22ab22d60f5ef57d7a96cdb
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-11 20:56:34 +0000

    Reset to f46a09dcea19d8c6e003aaef34e4c49651e7c798 and made changes for LLM Round 1

--- wat/__init__.py
+++ wat/__init__.py
@@ -2,7 +2,10 @@ import sys
 from .inspection.inspection import wat
 from .version import __version__
 
-__all__ = ['wat', '__version__']
+__all__ = [
+    'wat',
+    '__version__'
+]
 
 wat.__version__ = __version__
 
