91b0cf050c4905713969b084227375e7aa6ae533
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-11 22:04:22 +0000

    Reset to fe0ca7a76232896a0107202a7a6338df8c340ce1 and made changes for LLM Round 1

--- wat/inspection/insta/regenerate.py
+++ wat/inspection/insta/regenerate.py
@@ -5,25 +5,24 @@ from wat.inspection.insta.dump import dump_snippet
 from wat.inspection.insta.instaload import code
 
 
-def _regenerate(src_filename: str, dst_filenames: list[str]):
-    old_code: str = code.decode()
-    new_code: str = dump_snippet(src_filename)
-    replaced_contents: list[str] = []
-    
+def _regenerate(src_filename, dst_filenames):
+    old_code = code.decode()
+    new_code = dump_snippet(src_filename)
+    if old_code == new_code:
+        print('Insta-Load code is up to date')
+        return
+    replaced_contents = []
     for dst_filename in dst_filenames:
         content = Path(dst_filename).read_text()
         assert content.count(old_code) == 1, f'cannot find current Insta-Load code in {dst_filename}'
         replaced_content = content.replace(old_code, new_code)
+        assert replaced_content.count(old_code) == 0
         assert replaced_content.count(new_code) == 1
         replaced_contents.append(replaced_content)
 
-    if old_code == new_code:
-        print('Insta-Load code is up to date')
-        return
-
     for i, dst_filename in enumerate(dst_filenames):
-        Path(dst_filenames[i]).write_text(replaced_contents[i])
-        print(f'Code replaced in {dst_filenames[i]}')
+        Path(dst_filename).write_text(replaced_contents[i])
+        print(f'Code replaced in {dst_filename}')
 
 
 if __name__ == '__main__':
