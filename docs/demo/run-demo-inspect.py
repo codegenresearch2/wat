2cdb3b8731bbfaae4414e565fad0c85701ad30e5
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-11 20:00:24 +0000

    Reset to f46a09dcea19d8c6e003aaef34e4c49651e7c798 and made changes for LLM Round 1

--- docs/demo/run-demo-inspect.py
+++ docs/demo/run-demo-inspect.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 from livekey import animate_commands
 
-
 commands = [
     'python',
-    'import wat',
+    'from wat import wat',
     'wat / {7}',
     'wat.short / (1,)',
     'import re',
@@ -14,6 +13,17 @@ commands = [
     'wat.short / locals()',
 ]
 
+class Wat:
+    def __init__(self, code=None):
+        self.code = code
+
+    def __call__(self, *args, **kwargs):
+        if self.code:
+            return self.code(*args, **kwargs)
+
+wat = Wat()
+wat.code = re.match
+wat.short = locals()
 
 if __name__ == '__main__':
     animate_commands(commands, key_delay=0.1, line_delay=1.0)
