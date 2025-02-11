1aa94dded03cfcb45b33685f2c3a620e4917a5f1
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-11 23:36:52 +0000

    Reset to 79bb727e0eb62adcc8521f15120603a58f25b5c2 and made changes for LLM Round 1

--- wat/inspection/insta/dump.py
+++ wat/inspection/insta/dump.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import re
 import sys
 import zlib
-from typing import Any, Dict, List, Optional, Type, Iterable, Union
+from typing import List
 
 
 def dump_snippet(filename: str) -> str:
