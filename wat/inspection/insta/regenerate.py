985487b735a8e488423ac807d0739bc68714e953
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-11 22:05:34 +0000

    Reset to fe0ca7a76232896a0107202a7a6338df8c340ce1 and made changes for LLM Round 1

--- wat/inspection/insta/regenerate.py
+++ wat/inspection/insta/regenerate.py
@@ -7,9 +7,7 @@ from wat.inspection.insta.instaload import code
 def _regenerate(src_filename: str, dst_filenames: list[str]):
     old_code: str = code.decode()
     new_code: str = dump_snippet(src_filename)
-    if old_code == new_code:
-        print('Insta-Load code is up to date')
-        return
+    
     replaced_contents: list[str] = []
     for dst_filename in dst_filenames:
         content = Path(dst_filename).read_text()
@@ -22,6 +20,9 @@ def _regenerate(src_filename: str, dst_filenames: list[str]):
     for i, dst_filename in enumerate(dst_filenames):
         Path(dst_filenames[i]).write_text(replaced_contents[i])
         print(f'Code replaced in {dst_filenames[i]}')
+
+    if old_code == new_code:
+        print('Insta-Load code is up to date')
 
 if __name__ == '__main__':
     _regenerate(sys.argv[1], sys.argv[2:])
