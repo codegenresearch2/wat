c9f9b41740f37abe0055f59051ade7a106db6adb
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-11 22:59:44 +0000

    Reset to 1d277643cf7a2f9e91421b6b85d8e36423291ed7 and made changes for LLM Round 1

--- wat/inspection/inspection.py
+++ wat/inspection/inspection.py
@@ -80,7 +80,7 @@ def _yield_inspect_lines(obj, config: InspectConfig) -> Iterable[str]:
         yield f'signature: {signature}'
     
     if config.caller:
-        yield from _retrieve_caller_info()
+        yield from _generate_caller_info()
 
     doc = _get_doc(obj, long=True)
     if doc and not config.nodocs and callable(obj):
@@ -242,7 +242,7 @@ def _get_parent_types(type_: Type) -> Iterable[str]:
             yield _format_type(base_type)
 
 
-def _retrieve_caller_info() -> Iterable[str]:
+def _generate_caller_info() -> Iterable[str]:
     frame = _caller_stack_frame(6)
     if frame:
         frameinfo = inspect.getframeinfo(frame)
