649a5f1cedb09ed33ed3580db46ffb4d631b7e43
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-12 00:04:11 +0000

    Reset to b9254cb82e3fea37aa9649eddcdf5fcc2b58ec3b and made changes for LLM Round 1

--- tests/inspection/test_instaload.py
+++ tests/inspection/test_instaload.py
@@ -7,7 +7,7 @@ from tests.asserts import assert_multiline_match, StdoutCap
 
 def test_load_instaload_snippet():
     snippet = zlib.decompress(base64.b64decode(code)).decode()
-    assert '\nwat=Wat()\n' in snippet  # Removed spaces around the equals sign
+    assert '\nwat=Wat()\n' in snippet  # Ensured no spaces around the equals sign
     
     exec(snippet, globals())
     with StdoutCap() as capture:
