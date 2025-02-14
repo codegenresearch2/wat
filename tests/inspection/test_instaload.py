e4999b00a2814dfa40c8c9a59f199f0627916626
vatsal_venkatkrishna <vatsal_venkatkrishna@gcpl4-eu-0>
2025-02-14 12:43:02 +0000

    Reset to b9254cb82e3fea37aa9649eddcdf5fcc2b58ec3b and made changes for LLM Round 1

--- tests/inspection/test_instaload.py
+++ tests/inspection/test_instaload.py
@@ -1,19 +1,12 @@
 import base64
 import zlib
-
 from wat.inspection.insta.instaload import code
 from tests.asserts import assert_multiline_match, StdoutCap
 
-
 def test_load_instaload_snippet():
     snippet = zlib.decompress(base64.b64decode(code)).decode()
-    assert '\nwat=Wat()\n' in snippet
-    
+    assert '\nwat = Wat()\n' in snippet
     exec(snippet, globals())
     with StdoutCap() as capture:
         wat.short / 'moo'
-    assert_multiline_match(capture.output(), r'''
-value: 'moo'
-type: str
-len: 3
-''')
+    assert_multiline_match(capture.output(), r'''\nvalue: 'moo'\ntype: str\nlen: 3\n''')
